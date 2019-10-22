1206abb582f244789b57c1b62641ecd82b016ae9
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-10-22 11:32:52 +0300

    created folder templates and admin user to register

--- pics/admin.py
+++ pics/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Photographer,Photo,Location
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    
+
 
     admin.site.register(Photographer)
     admin.site.register(Photo)
